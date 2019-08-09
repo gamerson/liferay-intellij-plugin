24462d73fadb7be99f8deb3c6b37648f163f0c43
Terry Jia <terry.jia@liferay.com>
2019-08-09 16:15:20 +0800

    INTELLIJ-80 add liferay spring mvc portlet wizard

--- src/main/java/com/liferay/ide/idea/util/LiferayWorkspaceUtil.java
+++ src/main/java/com/liferay/ide/idea/util/LiferayWorkspaceUtil.java
@@ -14,7 +14,6 @@
 
 package com.liferay.ide.idea.util;
 
-import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.externalSystem.model.DataNode;
 import com.intellij.openapi.externalSystem.model.ExternalProjectInfo;
 import com.intellij.openapi.externalSystem.model.ProjectKeys;
@@ -31,6 +30,7 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.StringReader;
 
@@ -99,18 +99,12 @@ public class LiferayWorkspaceUtil {
 
 	@Nullable
 	public static String getLiferayVersion(Project project) {
-		PropertiesComponent propertiesComponent = PropertiesComponent.getInstance(project);
-
-		String liferayVersion = propertiesComponent.getValue(WorkspaceConstants.WIZARD_LIFERAY_VERSION_FIELD);
-
-		if (liferayVersion != null) {
-			return liferayVersion;
-		}
+		String liferayVersion = WorkspaceConstants.LIFERAY_VERSIONS[0];
 
 		VirtualFile projectRoot = getWorkspaceVirtualFile(project);
 
 		if (projectRoot == null) {
-			return null;
+			return liferayVersion;
 		}
 
 		VirtualFile settingsVirtualFile = projectRoot.findFileByRelativePath("/.blade.properties");
@@ -118,10 +112,11 @@ public class LiferayWorkspaceUtil {
 		if (settingsVirtualFile != null) {
 			Properties props = new Properties();
 
-			try {
-				props.load(settingsVirtualFile.getInputStream());
+			try (InputStream inputStream = settingsVirtualFile.getInputStream()) {
+				props.load(inputStream);
 
-				liferayVersion = props.getProperty(WorkspaceConstants.BLADE_LIFERAY_VERSION_FIELD);
+				liferayVersion = props.getProperty(
+					WorkspaceConstants.BLADE_LIFERAY_VERSION_FIELD, WorkspaceConstants.LIFERAY_VERSIONS[0]);
 			}
 			catch (IOException ioe) {
 			}
@@ -302,6 +297,14 @@ public class LiferayWorkspaceUtil {
 		String location = project.getBasePath();
 
 		return _getGradleProperty(location, WorkspaceConstants.DEFAULT_TARGET_PLATFORM_VERSION_PROPERTY, null);
+	}
+
+	@Nullable
+	public static String getWarsDir(Project project) {
+		String location = project.getBasePath();
+
+		return _getGradleProperty(
+			location, WorkspaceConstants.DEFAULT_WARS_DIR_PROPERTY, WorkspaceConstants.DEFAULT_WARS_DIR);
 	}
 
 	@Nullable
