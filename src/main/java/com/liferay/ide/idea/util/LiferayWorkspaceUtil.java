da4dbc6baa96b919b28118fbf6201d645694775b
Terry Jia <terry.jia@liferay.com>
2019-08-13 20:58:55 +0800

    INTELLIJ-42 normalize

--- src/main/java/com/liferay/ide/idea/util/LiferayWorkspaceUtil.java
+++ src/main/java/com/liferay/ide/idea/util/LiferayWorkspaceUtil.java
@@ -31,7 +31,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Properties;
 import java.util.regex.Matcher;
