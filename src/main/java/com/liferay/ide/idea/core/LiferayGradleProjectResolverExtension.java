c0e76004b1670d5994da0d15acf7628f42c10187
simon.jiang <simon.jiang@liferay.com>
2020-03-21 08:27:21 +0800

    INTELLIJ-90 avoid add target platform dependency for every gradle module

--- src/main/java/com/liferay/ide/idea/core/LiferayGradleProjectResolverExtension.java
+++ src/main/java/com/liferay/ide/idea/core/LiferayGradleProjectResolverExtension.java
@@ -61,6 +61,12 @@ public class LiferayGradleProjectResolverExtension extends AbstractProjectResolv
 
 		ExternalProject externalProject = _getExternalProject(gradleModule, resolverCtx);
 
+		File moduleDir = externalProject.getProjectDir();
+
+		ProjectData projectData = ideProject.getData();
+
+		String ideProjectFileDirectoryPath = projectData.getIdeProjectFileDirectoryPath();
+
 		if (resolverCtx.isResolveModulePerSourceSet()) {
 			final Map<String, Pair<DataNode<GradleSourceSetData>, ExternalSourceSet>> sourceSetMap =
 				ideProject.getUserData(GradleProjectResolver.RESOLVED_SOURCE_SETS);
@@ -79,42 +85,47 @@ public class LiferayGradleProjectResolverExtension extends AbstractProjectResolv
 						@NotNull ExternalSourceSet sourceSet) {
 
 						Collection<ExternalDependency> dependencies = sourceSet.getDependencies();
-						DomainObjectSet<? extends IdeaDependency> gradleDependencies = gradleModule.getDependencies();
 
-						for (IdeaDependency dependency : gradleDependencies) {
-							if (dependency instanceof InternalIdeaSingleEntryLibraryDependency) {
-								InternalIdeaSingleEntryLibraryDependency localDependency =
-									(InternalIdeaSingleEntryLibraryDependency)dependency;
-								DefaultExternalLibraryDependency libraryDependency =
-									new DefaultExternalLibraryDependency();
+						if (ideProjectFileDirectoryPath.equals(moduleDir.getAbsolutePath())) {
+							DomainObjectSet<? extends IdeaDependency> gradleDependencies =
+								gradleModule.getDependencies();
 
-								File jarFile = localDependency.getFile();
+							for (IdeaDependency dependency : gradleDependencies) {
+								if (dependency instanceof InternalIdeaSingleEntryLibraryDependency) {
+									InternalIdeaSingleEntryLibraryDependency localDependency =
+										(InternalIdeaSingleEntryLibraryDependency)dependency;
+									DefaultExternalLibraryDependency libraryDependency =
+										new DefaultExternalLibraryDependency();
 
-								String jarName = jarFile.getName();
+									File jarFile = localDependency.getFile();
 
-								if (jarName.endsWith(".jar")) {
-									GradleModuleVersion gradleModuleVersion = localDependency.getGradleModuleVersion();
+									String jarName = jarFile.getName();
 
-                                    assert gradleModuleVersion != null;
+									if (jarName.endsWith(".jar")) {
+										GradleModuleVersion gradleModuleVersion =
+											localDependency.getGradleModuleVersion();
 
-									libraryDependency.setName(gradleModuleVersion.getName());
+										assert gradleModuleVersion != null;
 
-									libraryDependency.setGroup(gradleModuleVersion.getGroup());
+										libraryDependency.setName(gradleModuleVersion.getName());
 
-									libraryDependency.setVersion(gradleModuleVersion.getVersion());
+										libraryDependency.setGroup(gradleModuleVersion.getGroup());
 
-									libraryDependency.setFile(jarFile);
+										libraryDependency.setVersion(gradleModuleVersion.getVersion());
 
-									libraryDependency.setSource(localDependency.getSource());
+										libraryDependency.setFile(jarFile);
 
-									InternalIdeaDependencyScope jarScope = localDependency.getScope();
+										libraryDependency.setSource(localDependency.getSource());
 
-									libraryDependency.setScope(jarScope.getScope());
+										InternalIdeaDependencyScope jarScope = localDependency.getScope();
 
-									libraryDependency.setExported(localDependency.getExported());
+										libraryDependency.setScope(jarScope.getScope());
+
+										libraryDependency.setExported(localDependency.getExported());
+									}
+
+									dependencies.add(libraryDependency);
 								}
-
-								dependencies.add(libraryDependency);
 							}
 						}
 
